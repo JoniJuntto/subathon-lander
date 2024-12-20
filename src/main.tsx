90d2685593e9dd15a4646320da82a24ad0370fe8
Joni Juntto <joni.juntto@gmail.com>
2024-12-21 01:31:14 +0200

    lol

--- src/main.tsx
+++ src/main.tsx
@@ -4,24 +4,32 @@ import { BrowserRouter, Route, Routes } from "react-router";
 import Frontpage from "@/pages/frontpage";
 import RootLayout from "@/layout";
 import Particles from "@/components/ui/particles";
-
 import "./index.css";
+import { PostHogProvider } from "posthog-js/react";
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+};
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Particles
-        className="absolute min-h-screen inset-0 z-0"
-        quantity={100}
-        ease={80}
-        color={"#fff"}
-        refresh
-      />
-      <Routes>
-        <Route element={<RootLayout />}>
-          <Route index element={<Frontpage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <PostHogProvider
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <BrowserRouter>
+        <Particles
+          className="absolute min-h-screen inset-0 z-0"
+          quantity={100}
+          ease={80}
+          color={"#fff"}
+          refresh
+        />
+        <Routes>
+          <Route element={<RootLayout />}>
+            <Route index element={<Frontpage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PostHogProvider>
   </StrictMode>
 );
